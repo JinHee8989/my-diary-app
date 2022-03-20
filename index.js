0abd7e0f0ed7ae066c58e5401c9a909e63f924be
JinHee8989 <wlsgml8989@gmail.com>
2022-03-20 22:44:14 +0900

    오타 수정

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import {name as appName} from './app.json';
 
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux'; 
-import promiseMiddleware from 'redux-promises';
+import promiseMiddleware from 'redux-promise';
 import reducers from './app/store/reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ //redux개발자 도구와 미들웨어를 같이 쓰기 위해 작성 
@@ -19,9 +19,9 @@ const createStoreWithMiddleware = createStore(reducers,composeEnhancers(
     applyMiddleware(promiseMiddleware)
 ))
 
-const appRedux = () =>
-<Provider store={createStoreWithMiddleware}> 
-    <App/>
-</Provider>
-
+const appRedux = () => (
+    <Provider store={createStoreWithMiddleware}> 
+        <App/>
+    </Provider>
+)
 AppRegistry.registerComponent(appName, () => appRedux);
