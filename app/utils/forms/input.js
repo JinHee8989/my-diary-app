3ebc9a6661d8eab3b54921acc817d03c651eda84
JinHee8989 <wlsgml8989@gmail.com>
2022-03-25 23:36:58 +0900

    commit

--- app/utils/forms/input.js
+++ app/utils/forms/input.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import {View, Image, StyleSheet, TextInput} from 'react-native';
 
-const input = (props) =>(
-    <TextInput
-        {...props}
-        style={styles.input}
+const input = (props) =>{
+    let template = null;
 
-    />
-)
+    switch(props.type){
+        case "textInput" : 
+            template = 
+            <TextInput
+                {...props}
+                style={styles.input}
+            />
+            
+            break;
+        case "textInputRevised" : 
+            template = 
+            <TextInput
+                {...props}
+                style={styles.inputRevised}
+            />    
+            break;
+        default:
+            return template;
+    }
+    return template;
+}
 
 const styles = StyleSheet.create({
     input:{
@@ -17,7 +34,15 @@ const styles = StyleSheet.create({
         fontSize:17,
         padding:5,
         marginTop:30,
+    },
+    inputRevised:{
+        width:'100%',
+        borderBottomWidth:3,
+        borderBottomColor:'red',
+        fontSize:17,
+        padding:5,
+        marginTop:30,
+
     }
-  
 });
 export default input;
