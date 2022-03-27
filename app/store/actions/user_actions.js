8a3696ed5bd7e9c204d717253fedb6ae576a59f1
JinHee8989 <wlsgml8989@gmail.com>
2022-03-27 23:42:23 +0900

    firebase 연동 작업1

--- app/store/actions/user_actions.js
+++ app/store/actions/user_actions.js
@@ -2,6 +2,10 @@ import {
     SIGN_IN,
     SIGN_UP
 } from '../types';
+import axios from 'axios'; //axios란 브라우저나 서버를 위한 http 클라이언트로 promise 형태임, 브라우저나 서버한테 request하기 위한 수단.
+import {
+    SIGNUP
+} from '../../utils/misc';
 
 export function signIn(data){
     return {
---
+++ app/utils/misc.js
@@ -0,0 +1,2 @@
+export const APIKEY = `AIzaSyClgpb2mPX9lAQcrXTETlq3iQGjMNcOkN0`;
+export const SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIKEY}`;
