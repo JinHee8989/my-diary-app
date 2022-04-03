dfeb57137c81ecda29e4e7af7c27a80b9813c841
JinHee8989 <wlsgml8989@gmail.com>
2022-04-03 16:32:13 +0900

    메인화면 작성

---
+++ app/utils/logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Image,Text} from 'react-native';
+
+const LogoTitle=()=>{
+    return(
+        <Image
+            source={require('../assets/images/winthiary_login_logo.png')}
+            style={{width:120, height:35}}
+            resizeMode="contain" />
+    )
+}
+
+export default LogoTitle; 
